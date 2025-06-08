556a7a245e65fc1d6beb2dc9de74949023ebc220
David Ernst <git@dsernst.com>
2025-06-08 14:27:39 -0700

    Install eslint-plugin-perfectionist

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,9 @@
 import { FlatCompat } from '@eslint/eslintrc'
+import perfectionist from 'eslint-plugin-perfectionist'
 
 const config = [
   ...new FlatCompat().extends('next/core-web-vitals', 'next/typescript'),
+  perfectionist.configs['recommended-natural'],
 ]
 
 export default config
