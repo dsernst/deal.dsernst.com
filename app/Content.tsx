d0ca4b86905033db461cabbdb9c4adbc6e79605b
David Ernst <git@dsernst.com>
2025-06-08 14:21:19 -0700

    Reduce layout jumps on iOS

--- app/Content.tsx
+++ app/Content.tsx
@@ -9,7 +9,10 @@ export function Content() {
   const [[odds1, odds2], setHashValues] = useUrlHashState()
 
   return (
-    <div className="min-h-screen p-8 flex flex-col items-center justify-center">
+    <div
+      style={{ minHeight: '100dvh' }} // ignore iOS bottom bar
+      className="min-h-screen p-8 pt-4 flex flex-col items-center justify-center"
+    >
       <h1 className="text-4xl font-bold mb-4">{title}</h1>
       <p className="text-lg text-gray-400 mb-8">{description}</p>
 
