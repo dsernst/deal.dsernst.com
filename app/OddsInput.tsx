8993cdd3310a531b9631ab02ac4a0128a265f7e4
David Ernst <git@dsernst.com>
2025-06-08 15:49:24 -0700

    Use type=number for OddsInput

--- app/OddsInput.tsx
+++ app/OddsInput.tsx
@@ -21,10 +21,11 @@ export default function OddsInput({
           </label>
           <div className="relative text-3xl">
             <input
-              type="text"
+              pattern="\d*"
+              type="number"
               {...{ placeholder }}
               autoFocus={index === 0 && !odds[0]}
-              className="px-3 py-2 h-20 w-30 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+              className="px-3 py-2 h-20 w-30 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 no-number-controls"
               onChange={(e) => {
                 const newValues: Odds = [odds[0], odds[1]]
                 newValues[index] = e.target.value
