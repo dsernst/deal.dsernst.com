168558d8ff9234e1ade01c1ad81a9a6292f8513b
David Ernst <git@dsernst.com>
2025-06-11 19:31:01 -0700

    UI: Show EVs using discount calculations

--- app/Calculation.tsx
+++ app/Calculation.tsx
@@ -84,10 +84,28 @@ export const Calculation = ({
           </span>
         </p>
 
-        <p className="text-xs mt-6 text-gray-500">
-          <i>Expected Value:</i>+FIX%
-        </p>
-        <p className="text-xs text-gray-700">for both sides</p>
+        {/* Expected Value */}
+        <div className="text-xs mt-6 text-gray-500 flex flex-col !gap-1 w-full *:flex *:gap-1 *:justify-between *:items-center">
+          <div className="self-center italic text-[10px]">Expected Value:</div>
+          <div className={usingLinear ? 'text-white/90' : ''}>
+            <span className="w-12">
+              {round(calculations.discounts.left.absolute * 100, 1)}¢
+            </span>
+            <span className="text-gray-500 text-[10px]">Absolute</span>
+            <span className="w-12 text-right">
+              {round(calculations.discounts.right.absolute * 100, 1)}¢
+            </span>
+          </div>
+          <div className={usingLinear ? '' : 'text-white/90'}>
+            <span className="w-12">
+              {round(calculations.discounts.left.relative * 100, 1)}%
+            </span>
+            <span className="text-gray-500 text-[10px]">Relative</span>
+            <span className="w-12 text-right">
+              {round(calculations.discounts.right.relative * 100, 1)}%
+            </span>
+          </div>
+        </div>
       </div>
     </div>
   )
