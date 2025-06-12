8c33b86d8e144077632eeb554e147f9e04b318f9
David Ernst <git@dsernst.com>
2025-06-11 20:04:23 -0700

    Tweak look of Expected Value section

--- app/Calculation.tsx
+++ app/Calculation.tsx
@@ -86,22 +86,28 @@ export const Calculation = ({
 
         {/* Expected Value */}
         <div className="text-xs mt-6 text-gray-500 flex flex-col !gap-1 w-full *:flex *:gap-1 *:justify-between *:items-center">
-          <div className="self-center italic text-[10px]">Expected Value:</div>
+          <div className="self-center font-bold text-[10px]">
+            Expected Value
+          </div>
           <div className={usingLinear ? 'text-white/90' : ''}>
             <span className="w-12">
+              <Plus />
               {round(calculations.discounts.left.absolute * 100, 1)}¢
             </span>
             <span className="text-gray-500 text-[10px]">Absolute</span>
             <span className="w-12 text-right">
+              <Plus />
               {round(calculations.discounts.right.absolute * 100, 1)}¢
             </span>
           </div>
           <div className={usingLinear ? '' : 'text-white/90'}>
             <span className="w-12">
+              <Plus />
               {round(calculations.discounts.left.relative * 100, 1)}%
             </span>
             <span className="text-gray-500 text-[10px]">Relative</span>
             <span className="w-12 text-right">
+              <Plus />
               {round(calculations.discounts.right.relative * 100, 1)}%
             </span>
           </div>
@@ -116,3 +122,5 @@ const colors = { NO: 'text-red-500/60', YES: 'text-green-500/50' }
 const Label = ({ label }: { label: Label }) => (
   <div className={`text-sm ${colors[label]}`}>{label}</div>
 )
+
+const Plus = () => <span className="text-[10px] relative bottom-px">+</span>
