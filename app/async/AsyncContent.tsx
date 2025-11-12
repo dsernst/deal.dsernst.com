da19c485c2c87097b372c960c9c6a224cb03c710
David Ernst <git@dsernst.com>
2025-11-12 02:01:47 -0800

    Create /async page, copied from homepage

---
+++ app/async/AsyncContent.tsx
@@ -0,0 +1,46 @@
+'use client'
+
+import { useState } from 'react'
+
+import { Calculation } from '../Calculation'
+import { description, title } from '../constants'
+import { ModeContainer } from '../ModeSwitcher'
+import { OneShotAgreement } from '../OneShotAgreement'
+import { PrivateInput } from '../PrivateInput'
+
+export type Inputs = [string, string]
+export function Content() {
+  const [[input1, input2], setValues] = useState<Inputs>(['', ''])
+
+  return (
+    <div
+      className="min-h-screen p-8 pt-4 flex flex-col items-center justify-center"
+      style={{ minHeight: '100dvh' }} // ignore iOS bottom bar
+    >
+      <h1 className="text-4xl font-bold mb-1">{title}</h1>
+      <p className="text-lg text-gray-400 mb-8">{description}</p>
+
+      {/* Private inputs */}
+      <PrivateInput {...{ inputs: [input1, input2], setValues }} />
+
+      {/* Results */}
+      <ModeContainer tabs={['Report Overlap Only', 'Pick Fair Price']}>
+        {({ ModeSwitcher, overlapOnly }) => (
+          <>
+            <Calculation {...{ input1, input2, overlapOnly }} />
+            <OneShotAgreement {...{ ModeSwitcher, overlapOnly }} />
+          </>
+        )}
+      </ModeContainer>
+
+      {/* Learn more */}
+      <a
+        className="text-sm text-gray-400 mt-16 block hover:underline"
+        href="https://github.com/dsernst/deal.dsernst.com#dealdsernstcom"
+        target="_blank"
+      >
+        Learn more
+      </a>
+    </div>
+  )
+}
